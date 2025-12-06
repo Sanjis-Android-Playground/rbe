9692400462ae0e0df0ae720a2431192ac6fecb50
Sanjivns <113604406+Sanjivns@users.noreply.github.com>
2025-12-06 15:57:07 +0000

    fix for axion

--- install_rbe.sh
+++ install_rbe.sh
@@ -71,6 +71,45 @@ export RBE_JAVA_POOL=default
 export RBE_METALAVA_POOL=default
 export RBE_LINT_POOL=default
 
+# Completely disable remote Java toolchain (unreliable)
+export RBE_JAVAC=0
+export RBE_JAR=0
+export RBE_D8=0
+export RBE_TURBINE=0
+
+export RBE_javac=0
+export RBE_jar=0
+export RBE_d8=0
+export RBE_turbine=0
+
+export RBE_JAVAC_EXEC_STRATEGY=local
+export RBE_JAR_EXEC_STRATEGY=local
+
+# Tools that are ALWAYS local-only
+export RBE_HIDDENAPI=0
+export RBE_HIDDENAPI_EXEC_STRATEGY=local
+export RBE_METALAVA=0
+export RBE_METALAVA_EXEC_STRATEGY=local
+export RBE_LINT=0
+export RBE_LINT_EXEC_STRATEGY=local
+export RBE_ZIP=0
+export RBE_ZIP_EXEC_STRATEGY=local
+export RBE_SIGNAPK=0
+export RBE_SIGNAPK_EXEC_STRATEGY=local
+
+# Heavy tasks that SHOULD remain remote
+export RBE_CXX=1
+export RBE_CXX_EXEC_STRATEGY=remote_local_fallback
+export RBE_CXX_LINKS=1
+export RBE_CXX_LINKS_EXEC_STRATEGY=remote_local_fallback
+export RBE_R8=1
+export RBE_R8_EXEC_STRATEGY=remote_local_fallback
+
+# Safe RBE settings
+export RBE_use_unified_downloads=false
+export RBE_use_unified_uploads=false
+export RBE_remote_download_mode=minimal
+
 # Git optimization (optional)
 export GIT_ALTERNATE_OBJECT_DIRECTORIES=/dev/shm/git-tmp
 EOF
